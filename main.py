0d4a5697674f9ef0917e183125156990467aaca2
Dashpreet Singh <104725945+DashpreetSingh@users.noreply.github.com>
2023-05-04 11:32:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from fileinput import filename
 import urllib.request
+import os
 
-with open("C:\\Users\\KUNAL SINGH\\OneDrive\\Desktop\\web\\RBtextUrl.txt","r") as a_file:
+with open(r"C:\Users\Lenovo\Desktop\IMAGE_BLUR\imagezydus.txt") as a_file:
     for line in a_file:
-        stripped_line = 5000
         stripped_line = line.strip()
 
-        filename = stripped_line[52:]
-        urllib.request.urlretrieve(stripped_line,"C:\\Users\\KUNAL SINGH\\OneDrive\\Desktop\\web\\UrlImagedata"+filename)
+        # extract filename from URL
+        filename = os.path.basename(stripped_line)
 
+        # download and save image
+        output_path = os.path.join(r"C:\Users\Lenovo\Desktop\IMAGE_BLUR\zydus_images", filename)
+        urllib.request.urlretrieve(stripped_line, output_path)
 
         print(filename)
